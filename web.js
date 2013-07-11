4deae40017de844d18efdaf5dd5b710abbbd38b8
Chris Sears <csears@gmail.com>
2013-07-11 23:06:10 +0000

    Fixed sync vs non-sync change

--- web.js
+++ web.js
@@ -4,10 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function (err, data) {
-    if (err) throw err;
-    response.send(data.toString());
-  });
+  data = fs.readFileSync('index.html');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
